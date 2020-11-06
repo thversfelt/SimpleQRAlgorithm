43105421a2f4a24852af85fd243380eb86785082
thversfelt <thversfelt@hotmail.com>
2020-11-06 13:30:25 -0500

    Initial commit.

---
+++ LinearAlgebra.cs
@@ -0,0 +1,205 @@
+﻿using System;
+
+namespace SimpleQRAlgorithm
+{
+    public static class LinearAlgebra
+    {
+        /// <summary>
+        /// Gets the j-th column of the given matrix.
+        /// </summary>
+        /// <param name="A">The matrix.</param>
+        /// <param name="j">The index of the column.</param>
+        /// <returns>The column vector.</returns>
+        public static float[] GetColumn(float[,] A, int j)
+        {
+            int n = A.GetLength(0);
+            float[] column = new float[n];
+            for (int i = 0; i < n; i++)
+            {
+                column[i] = A[i, j];
+            }
+            return column;
+        }
+
+        /// <summary>
+        /// Duplicates the given matrix.
+        /// </summary>
+        /// <param name="A">The matrix to duplicate.</param>
+        /// <returns>The duplicated matrix.</returns>
+        public static float[,] Duplicate(float[,] A)
+        {
+            int n = A.GetLength(0);
+            float[,] B = new float[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    B[i, j] = A[i, j];
+                }
+            }
+
+            return B;
+        }
+
+        /// <summary>
+        /// Scales the given vector by a scalar.
+        /// </summary>
+        /// <param name="a">The vector to scale.</param>
+        /// <param name="s">The scalar.</param>
+        /// <returns></returns>
+        public static float[] Scale(float[] a, float s)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] *= s;
+            }
+            return a;
+        }
+
+        /// <summary>
+        /// Calculates the inner product between two vectors.
+        /// </summary>
+        /// <param name="a">The first vector.</param>
+        /// <param name="b">The second vector.</param>
+        /// <returns>The inner product.</returns>
+        public static float InnerProduct(float[] a, float[] b)
+        {
+            float product = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                product += a[i] * b[i];
+            }
+            return product;
+        }
+
+        /// <summary>
+        /// Projects the vector "a" orthogonally onto vector "u".
+        /// </summary>
+        /// <param name="a">The vector to project.</param>
+        /// <param name="b">The vector on which will be projected.</param>
+        /// <returns>The projection.</returns>
+        public static float[] Project(float[] a, float[] b)
+        {
+            return Scale(a, InnerProduct(a, b) / InnerProduct(a, a));
+        }
+
+        /// <summary>
+        /// Substracts vector "b" from vector "a".
+        /// </summary>
+        /// <param name="a">The vector to be subtracted.</param>
+        /// <param name="b">The substracting vector.</param>
+        /// <returns>The substracted vector.</returns>
+        public static float[] Subtract(float[] a, float[] b)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] -= b[i];
+            }
+            return a;
+        }
+
+        /// <summary>
+        /// Multiplies matrix A with matrix B.
+        /// </summary>
+        /// <param name="A">The first matrix.</param>
+        /// <param name="B">The second matrix.</param>
+        /// <returns>The resulting matrix.</returns>
+        public static float[,] Product(float[,] A, float[,] B)
+        {
+            int n = A.GetLength(0);
+            float[,] C = new float[n, n];
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    for (int k = 0; k < n; ++k)
+                    {
+                        C[i, j] += A[i, k] * B[k, j];
+                    }
+                }
+            }
+
+            return C;
+        }
+
+        /// <summary>
+        /// Calculates the transpose of the given matrix.
+        /// </summary>
+        /// <param name="A">The matrix to transpose.</param>
+        /// <returns>The transpose.</returns>
+        public static float[,] Transpose(float[,] A)
+        {
+            int n = A.GetLength(0);
+            float[,] B = new float[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    B[i, j] = A[j, i];
+                }
+            }
+
+            return B;
+        }
+
+        /// <summary>
+        /// Calculates the magnitude of the given vector.
+        /// </summary>
+        /// <param name="a">The vector to compute the magnitude of.</param>
+        /// <returns>The magnitude.</returns>
+        public static float Magnitude(float[] a)
+        {
+            float magnitude = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                magnitude += a[i] * a[i];
+            }
+            return (float)Math.Sqrt(magnitude);
+        }
+
+        /// <summary>
+        /// Prints the given matrix.
+        /// </summary>
+        /// <param name="A">The matrix to print.</param>
+        /// <returns>The string representation of the given matrix.</returns>
+        public static string ToString(float[,] A)
+        {
+            int n = A.GetLength(0);
+            string text = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 0) text += ',';
+
+                text += '{';
+                for (int j = 0; j < n; j++)
+                {
+                    if (j > 0) text += ',';
+                    text += A[i, j];
+                }
+                text += '}';
+            }
+            return text;
+        }
+
+        /// <summary>
+        /// Prints the given vector.
+        /// </summary>
+        /// <param name="a">The vector to print.</param>
+        /// <returns>The string representation of the given vector.</returns>
+        public static string ToString(float[] a)
+        {
+            string text = "{";
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i > 0) text += ',';
+                text += a[i];
+            }
+            text += '}';
+            return text;
+        }
+    }
+}
---
+++ QRAlgorithm.cs
@@ -0,0 +1,83 @@
+﻿namespace SimpleQRAlgorithm
+{
+    public static class QRAlgorithm
+    {
+        /// <summary>
+        /// Runs the QR algorithm to find the eigenvalues of the given matrix (see https://en.wikipedia.org/wiki/QR_algorithm).
+        /// </summary>
+        /// <param name="A">The matrix for which eigenvalues should be found.</param>
+        /// <param name="iterations">The number of iterations.</param>
+        /// <returns>The eigenvalues of the given matrix.</returns>
+        public static float[] Run(float[,] A, int iterations)
+        {
+            float[,] B = LinearAlgebra.Duplicate(A);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                QRDecomposition(B, out float[,] Q, out float[,] R);
+                B = LinearAlgebra.Product(R, Q);
+            }
+
+            // The eigenvalues are on the diagonal of the matrix.
+            int n = A.GetLength(0);
+            float[] eigenvalues = new float[n];
+            for (int i = 0; i < n; i++)
+            {
+                eigenvalues[i] = B[i, i];
+            }
+
+            return eigenvalues;
+        }
+
+        /// <summary>
+        /// Calculates the QR decomposition of the given matrix A (see https://en.wikipedia.org/wiki/QR_decomposition). 
+        /// </summary>
+        /// <param name="A">The matrix to decompose.</param>
+        /// <param name="Q">The Q part of the decomposition.</param>
+        /// <param name="R">The R part of the decomposition.</param>
+        private static void QRDecomposition(float[,] A, out float[,] Q, out float[,] R)
+        {
+            int n = A.GetLength(0);
+
+            float[,] U = LinearAlgebra.Duplicate(A);
+
+            // Calculate the U matrix using the Gram–Schmidt process (see https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process).
+            for (int j = 1; j < n; j++)
+            {
+                float[] u = LinearAlgebra.GetColumn(U, j);
+                float[] v = LinearAlgebra.GetColumn(U, j);
+
+                for (int k = j - 1; k >= 0; k--)
+                {
+                    float[] uk = LinearAlgebra.GetColumn(U, k);
+                    u = LinearAlgebra.Subtract(u, LinearAlgebra.Project(uk, v));
+                }
+
+                // Update the column entries in U.
+                for (int i = 0; i < n; i++)
+                {
+                    U[i, j] = u[i];
+                }
+            }
+
+            // Normalize the column vectors of U.
+            for (int j = 0; j < n; j++)
+            {
+                float[] u = LinearAlgebra.GetColumn(U, j);
+                float magnitude = LinearAlgebra.Magnitude(u);
+
+                // Update the column entries in U.
+                for (int i = 0; i < n; i++)
+                {
+                    U[i, j] = u[i] / magnitude;
+                }
+            }
+
+            // The U matrix is now the Q part of the decomposition.
+            Q = U;
+
+            // Calculate the R part of the decomposition.
+            R = LinearAlgebra.Product(LinearAlgebra.Transpose(Q), A);
+        }
+    }
+}
